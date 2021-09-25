c5f97582a6676613368aaec9d51421c10c7ff400
sierraskyewallace <sskwallace@gmail.com>
2021-09-24 19:32:53 -0700

    added card layour:

--- drink.js
+++ drink.js
@@ -1,22 +1,33 @@
 class Drink {
     constructor(drink, drinkAttributes) {
-    this.id = drink.id;
-    this.name = drinkAttributes.name;
-    this.image_url = drinkAttributes.image_url;
-    this.ingredients = drinkAttributes.ingredients;
-    this.instructions = drinkAttributes.instructions;
+    this.id = drink.id
+    this.name = drinkAttributes.name
+    this.image_url = drinkAttributes.image_url
+    this.instructions = drinkAttributes.instructions
+    
   }
     
     renderDrink() {
         return `
-        <div class="drink-card">
-            <h2>${this.name}</h2>
-            <img src="${this.image_url}">
-                <ul>
-                    ${this.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
-                </ul>
-                <p>${this.instructions}</p>
-        </div> `
+        <div class="col-md-4">
+        <div class="card mb-4 shadow-sm">
+          <img src=${this.image_url} class="card-img-top" alt="...">
+          <div class="card-body">
+            <h4 class="card-title">${this.name}</h4>
+            <h5 class="card-title">${this.description}</h5>
+            <p class="card-text">${this.instructions}</p>
+            <ul>
+                <li>${this.ingredients}</li>
+            </ul>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
     }
     
 }
--- index.js
+++ index.js
@@ -1,32 +1,64 @@
-const BASE_URL = 'https://localhost:3000';
+const BASE_URL = 'http://localhost:3000/api/v1';
 const DRINKS_URL = `${BASE_URL}/drinks`;
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
 
     getDrinks();
 
-    const createDrinkButton = document.querySelector('#create-form-button-container');
-    createDrinkButton.addEventListener('click', function (e) {
+    // hide create form on page load, show on click
+    const createForm = document.querySelector('#form-container');
+    createForm.addEventListener('submit', function (e) {
         e.preventDefault();
+        createFormHandler(e);
+    });
+
+
+    const createButton = document.querySelector('#create-form-button');
+    createButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        createForm.style.display = 'block';
+        createButton.style.display = 'none';
+
+    });
+
+    function createFormHandler(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const drink = {
+            name: formData.get('name'),
+            description: formData.get('description'),
+            image: formData.get('image')
+        };
+        fetch(DRINKS_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(drink)
+        })
+            .then(response => response.json())
+            .then(drink => {
+                let newDrink = new Drink(drink, drink.attributes);
+                document.querySelector('#drinks-container').innerHTML += newDrink.renderDrink();
+            });
+    };
         
-    });
-
-
-    const createDrinkForm = document.querySelector('#form-container').style.display = 'none';
-    createDrinkForm.addEventListener('submit', function (event) {
-        createDrinkFormHandler(event);
-    });
+            
 
     function getDrinks() {
         fetch(DRINKS_URL)
             .then(response => response.json())
             .then(drinks => {
                 drinks.data.forEach(drink => {
-                    let newDrink = new Drink(drink, drinkAtributes);
-                    newDrink.renderDrink();
+                    console.log(drinks);
+                    let newDrink = new Drink(drink, drink.attributes);
+                    document.querySelector('#drinks-container').innerHTML += newDrink.renderDrink();
                 });
             });
+    } });
 
-    }
-})
+
+                
+
