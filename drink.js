d5957d701048cfa52c3a01ae58205758df18489c
sierraskyewallace <sskwallace@gmail.com>
2021-09-24 22:53:05 -0700

    changed text on card

--- drink.js
+++ drink.js
@@ -12,18 +12,17 @@ class Drink {
     renderDrink() {
         return `
         <br><br>
-       <div class="row row-cols-1 row-cols-md-2"style="margin: 3em;">
-        <div class="col-md-4">
-        <div class="card text-center text-white bg-dark mb-10 w-60 h-35">
+        <div class="col-md-4"style="margin: 2em">
+        <div class="card text-center text-white bg-dark">
           <img src=${this.image_url} class="card-img-top"height="200" width="50"alt="...">
           <div class="card-body">
             <h3 class="card-title">${this.name}</h3>
             <h5 class="card-title">${this.description}</h5>
-            <p class="card-text">${this.instructions}</p>
             <ul>
                 <!--- ingredients nested -->
                 ${this.ingredients.map(ingredient => `<li>${ingredient.name}</li>`).join('')}
             </ul>
+            <p class="card-text">Instructions: ${this.instructions}</p>
             <div class="d-flex justify-content-center">
               <div class="btn-group">
               <button class="btn btn-light">
