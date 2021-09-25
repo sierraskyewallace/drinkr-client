3622365a0c36565ac4f425dd3e8f9948883d30c9
sierraskyewallace <sskwallace@gmail.com>
2021-09-24 17:05:07 -0700

    adding create drink form with ingredient select

--- index.js
+++ index.js
@@ -4,8 +4,29 @@ const DRINKS_URL = `${BASE_URL}/drinks`;
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    
+    getDrinks();
 
-});
+    const createDrinkButton = document.querySelector('#create-form-button-container');
+    createDrinkButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+    });
 
-    
+
+    const createDrinkForm = document.querySelector('#form-container').style.display = 'none';
+    createDrinkForm.addEventListener('submit', function (event) {
+        createDrinkFormHandler(event);
+    });
+
+    function getDrinks() {
+        fetch(DRINKS_URL)
+            .then(response => response.json())
+            .then(drinks => {
+                drinks.data.forEach(drink => {
+                    let newDrink = new Drink(drink, drinkAtributes);
+                    newDrink.renderDrink();
+                });
+            });
+
+    }
+})
--- notes.md
+++ notes.md
@@ -10,3 +10,4 @@ Read/view drinks,
 create new drinks with ingred and instruc and image
 edit drinks
 
+need create drink, add or create ingredient
