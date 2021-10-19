2291155efe8255088e694293a979c99548b7b608
sierraskyewallace <sskwallace@gmail.com>
2021-10-18 17:17:55 -0700

    done

--- drink.js
+++ drink.js
@@ -11,12 +11,15 @@ class Drink {
   }
     
     renderDrink() {
-        return `
+      return `
         
-        
-        <div class="col-md-4"style="margin: 2em;">
+        <div class="row">
+      
+
+        <div class="col-md-4"style="margin:2em;">
+       
         <div class="card text-left text-white bg-dark">
-        <img src=${this.image_url} class="card-img-top"height="220" width="50"alt="..."> <div class="card-body"> 
+        <img src=${this.image_url} class="card-img-top"height="200" width="20"alt="..."> <div class="card-body"> 
         <h3 class="card-title">${this.name}</h3> 
         <h6 class="card-title">Liqour | ${this.liqour.name}</h6>
         <br><br>
@@ -26,12 +29,10 @@ class Drink {
         <br><br> <p class="card-text">Instructions |</p>
         <p class="card-text"> ${this.instructions}</p> 
         </div>
+       </div>
+       
+        <br><br>
         </div>
-        </div>
-        </div>
-        </div>
-        </div>
-        <br><br>;
         `
     }
     
